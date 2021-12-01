9577953e37cc34d386cc193de151f7fb758d4190
JonathanRichards <jrich87@uwo.ca>
2021-12-01 14:54:08 -0500

    Fixing terminate

--- Client.cpp
+++ Client.cpp
@@ -7,129 +7,153 @@
 using namespace Sync;
 
 std::string name;
-bool done = false;
 std::vector<std::string> clientHistory;
 
-//refreshes  chat with clint history of messages
-void refreshMessages() {
-	system("clear");
-	for (std::string line : clientHistory) {
-		std::cout << line +"\n";
-	}
-        std::cout << "-----------------------------------\n";
-	std::cout << "Message: ";
-	std::cout << std::flush;
+int chatEnded = false; 
+
+// refreshes  chat with clint history of messages
+void refreshMessages()
+{
+   system("clear");
+   std::cout << "------------ CHAT ROOM HISTORY -----------------\n";
+   for (std::string line : clientHistory)
+   {
+      std::cout << line + "\n";
+   }
+   std::cout << "\n----------------------------------------------\n";
+   std::cout << "Message: (or done)\n";
+   std::cout << std::flush;
 }
 
 class ReadThread : public Thread
 {
 private:
-        //socket
-	Socket *socket;
+   // socket
+   Socket *socket;
 
 public:
-	//constructor - initalize socket
-	ReadThread(Socket* socketInput)
-		: socket(socketInput)
-	{
-	}
+   // constructor - initalize socket
+   ReadThread(Socket *socketInput)
+       : socket(socketInput)
+   {
+   }
 
-	//deconstructor - delete is handled in main function
-	~ReadThread(){}
+   // deconstructor - delete is handled in main function
+   ~ReadThread() {
+      // indicate the chat has ended
+      chatEnded = true; 
 
-	//main thread method
-	virtual long ThreadMain()
-	{
-                //response variable for socket
-		ByteArray response;
+   }
 
-                //loop until exited
-		while (true)
-		{
-			socket->Read(response);
-			std::string responseString = response.ToString();
-                        
-                        if(responseString == "done"){
-                                done = true;
-                                break;
-                        }
-                        //add responseString to local histroy and refresh
-			clientHistory.push_back(responseString);
-			refreshMessages();
-			
-		}
-		return 1;
-	}
+   // main thread method
+   virtual long ThreadMain()
+   {
+      // response variable for socket
+      ByteArray response;
+
+      // loop until exited
+      while ( ! chatEnded )
+      {
+         if (socket->Read(response) == 0)
+         {
+            std::cout << "\nConnection lost, press enter to close chat.";
+            break;
+         }
+         std::string responseString = response.ToString();
+
+         if (responseString == "done")
+         {
+            std::cout << "\nPartner ended chat, press enter to close chat.";
+            break;
+         }
+         // add responseString to local histroy and refresh
+         clientHistory.push_back(responseString);
+         refreshMessages();
+      }
+      // indicate the chat has ended
+      chatEnded = true; 
+
+      return 1;
+   }
 };
 
 int main(void)
 {
-	// Welcome the user
-	std::cout << "SE3313 Chat Room Client" << std::endl;
+   // Welcome the user
+   std::cout << "SE3313 Chat Room Client" << std::endl;
 
-	// Create our socket
-	Socket socket("127.0.0.1", 3000);
+   // Create our socket
+   Socket socket("127.0.0.1", 3000);
 
-	// Initially we need a connection
-	while (true)
-	{
-		try
-		{
-			std::cout << "Connecting...";
-			std::cout.flush();
-			socket.Open();
-			std::cout << "OK\n" << std::endl;
-                        break;
-		}
-		catch (...)
-		{
-		}
+   // Initially we need a connection
+   while (true)
+   {
+      try
+      {
+         std::cout << "Connecting...";
+         std::cout.flush();
+         socket.Open();
+         std::cout << "OK\n"<< std::endl;
+         break;
+      }
+      catch (...)
+      {
+      }
 
-		// Try again every 5 seconds
-		std::cout << "Trying again in 5 seconds" << std::endl;
-		sleep(5);
-	}
+      // Try again every 5 seconds
+      std::cout << "Trying again in 5 seconds" << std::endl;
+      sleep(5);
+   }
 
-	std::cout<< "Enter Name: ";
-	std::getline(std::cin, name);
+   std::cout << "Enter Name: ";
+   std::getline(std::cin, name);
 
-	//command to get messages/start thred
-        //socket.Write();
+   // start read thread for client
+   ReadThread *readThread = new ReadThread(&socket);
 
-        //start read thread for client
-	ReadThread* readThread = new ReadThread(&socket);
-        //input variable
-	std::string input;
+   // continuely ask for input
+   std::string input;
+   
+   // Send first message to indicate who is connected 
+   if ( socket.Write(name + " is connected!") == 0) {
+      chatEnded = true; 
+   }
+   sleep(1);
 
-	//continuely ask for input
-	while (!done)
-	{
-		//get input
-		std::getline(std::cin, input);
+   // continue to prompt the user for next message and send to partner         
+   while ( !chatEnded )
+   {
+      // get input
+      std::getline(std::cin, input);
+ 
+      // check to see if the chat session has ended
+      if ( chatEnded ) {
+         break;
+      } 
+      // check is user is asking for chat session to end 
+      else if (input == "done")
+      {
+         socket.Write(input);
+         std::cout << "\n Ending chat session...\n";
+         break;
+      } 
+      else
+      {
+         // send input to server as user:message
+         if (socket.Write(name + ": " + input) == 0)
+         {
+            std::cout << "Connection lost, closing chat.\n";
+            break;
+         }
+         clientHistory.push_back("You: " + input);
+         refreshMessages();
+      }
 
-                if(input == "done"){
-                        socket.Write(input);
-                        done = true;
-                        break;
-                } else {
-                        //send input to server as user:message
-		        socket.Write(name + ": " +input);
-                        clientHistory.push_back(name + ": " +input);
-			refreshMessages();
-                }
+   }
 
-
-		//if user wants to end, send it to server as well, then end client
-		if (input == "done") 
-		{
-			//terimate
-			std::cout << "\n Ending client session..." << '\n';
-			delete readThread;
-			break;
-		}
-	}
-
-	//close socket
-	socket.Close();
-	return 0;
+   // close socket
+   socket.Close();
+   delete readThread;
+   std::cout << "\nChat Ended.\n";
+   return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -12,131 +12,157 @@ using namespace Sync;
 class SocketThread : public Thread
 {
 private:
-    //reference to our connected sockets
-    Socket& socket;
-    Socket& connectedSocket;
+   int endThread; 
 
-    // End the thread
-    bool& endThread;
+   // reference to our connected sockets
+   Socket &socket;
+   Socket &connectedSocket;
+
 public:
-    SocketThread(Socket& socket1,Socket& socket2,bool& endThread)
-    : socket(socket1), connectedSocket(socket2), endThread(endThread){}
+   SocketThread(Socket &socket1, Socket &socket2, bool &endThread)
+       : socket(socket1), connectedSocket(socket2), endThread(endThread) {}
 
-    ~SocketThread()
-    {}
+   ~SocketThread()
+   {
+   }
 
-    Socket& GetSocket()
-    {
-        return socket;
-    }
+   Socket &GetSocket()
+   {
+      return socket;
+   }
 
+   virtual long ThreadMain()
+   {
+      ByteArray data;
+      while ( !endThread )
+      {
+         try
+         {
+            // read from socket
+            if ( socket.Read(data) == 0 ) {
+               // socket is closed, shutdown thread
+               std::cout << "Socket closed on READ" << std::endl;
+               break; 
+            };
 
-    virtual long ThreadMain()
-    {
-        ByteArray data;
-        while(!endThread)
-        {
-            try
-            {
-                //read from socket
-                socket.Read(data);
-
-                //send to other
-                connectedSocket.Write(data);
+            // send to other
+            if ( connectedSocket.Write(data) == 0 ) {
+               // socket is closed, shutdown thread
+               std::cout << "Socket closed on WRITE" << std::endl;
+               break; 
             }
-            catch (...)
-            {
-                endThread = true;
-            }
-        }
-        return 0;
-    }
+         }
+         catch (...)
+         {
+            // unknown error occurred, shutting down thread.    
+            break;
+         }
+      }
+
+      std::cout << "Shutting down thread" << std::endl;
+
+      // flag to partner that the chat should end
+      endThread = true; 
+
+      // ensure both sockets are closed to trigger the clients apps to close
+      socket.Close(); 
+      connectedSocket.Close(); 
+
+      return 0;
+   }
 };
 
-//handles server operations
+// handles server operations
 class ServerThread : public Thread
 {
 private:
-    SocketServer& server;
-    std::vector<SocketThread*> socketThreads;
-    bool terminate = false;
+   SocketServer &server;
+   std::vector<SocketThread *> socketThreads;
+   bool terminate = false;
+
 public:
-    ServerThread(SocketServer& server,std::vector<SocketThread*>  socketThreads)
-    : server(server), socketThreads(socketThreads)
-    {}
+   ServerThread(SocketServer &server, std::vector<SocketThread *> socketThreads)
+       : server(server), socketThreads(socketThreads)
+   {
+   }
 
-    ~ServerThread()
-    {
-        for(SocketThread* thread : socketThreads){
-            try {
-                // Close the sockets
-                std::cout<<"Closing socket...\n";
-                Socket& toClose = thread->GetSocket();
-                toClose.Close();
-                delete thread;
-            }
-            catch (...){
-                terminate = true;
-            }
-        }
+   ~ServerThread()
+   {
+      for (SocketThread *thread : socketThreads)
+      {
+         try
+         {
+            // Close the sockets
+            Socket &toClose = thread->GetSocket();
+            std::cout << "Closing Client Socket - " << toClose.GetFD() << "...\n" ;
+            toClose.Close();
+            std::cout << "Deleting Thread \n";
+            delete thread;
+         }
+         catch (...)
+         {
+            terminate = true;
+         }
+      }
 
-        terminate = true;
-    }
+      terminate = true;
+   }
 
-    virtual long ThreadMain()
-    {
-        while(!terminate){
-            try {
-                std::cout << "Server awaiting connections" << std::endl;
-                // Wait for client one socket connection
-                Socket* connectionOne = new Socket(server.Accept());
-                Socket& socketReferenceOne = *connectionOne;
-                socketReferenceOne.Write(ByteArray("Success!\n"));
-                std::cout << "1st Connection received. Awaiting second connection." << std::endl;
+   virtual long ThreadMain()
+   {
+      while (!terminate)
+      {
+         try
+         {
+            std::cout << "Server awaiting connections" << std::endl;
+            // Wait for client one socket connection
+            Socket *connectionOne = new Socket(server.Accept());
+            Socket &socketReferenceOne = *connectionOne;
+            socketReferenceOne.Write(ByteArray("Success!"));
+            std::cout << "1st Connection received. Awaiting second connection." << std::endl;
 
-                // Wait for client two socket connection
-                Socket* connectionTwo = new Socket(server.Accept());
-                Socket& socketReferenceTwo = *connectionTwo;
-                socketReferenceTwo.Write(ByteArray("Success!\n"));
-                std::cout << "2nd Connection received. Initializing chat." << std::endl;
+            // Wait for client two socket connection
+            Socket *connectionTwo = new Socket(server.Accept());
+            Socket &socketReferenceTwo = *connectionTwo;
+            socketReferenceTwo.Write(ByteArray("Success!\n"));
+            std::cout << "2nd Connection received. Initializing chat." << std::endl;
 
-                //send both clients connected
-                socketReferenceOne.Write(ByteArray("Connected!\n"));
-                sleep(1);
-                socketReferenceTwo.Write(ByteArray("Connected!\n"));
+            // send both clients connected
+            socketReferenceOne.Write(ByteArray("Connected."));
+            sleep(1);
+            socketReferenceTwo.Write(ByteArray("Connected.\n"));
 
-                // Pass a reference to this pointer into a new socket thread
-                socketThreads.push_back(new SocketThread(socketReferenceOne, socketReferenceTwo, terminate));
-                socketThreads.push_back(new SocketThread(socketReferenceTwo, socketReferenceOne, terminate));
-            }
-            catch (...){
-                terminate = true;
-            }
-        }
-        return 1;
-    }
+            // Pass a reference to this pointer into a new socket thread
+            socketThreads.push_back(new SocketThread(socketReferenceOne, socketReferenceTwo, terminate));
+            socketThreads.push_back(new SocketThread(socketReferenceTwo, socketReferenceOne, terminate));
+         }
+         catch (...)
+         {
+            terminate = true;
+         }
+      }
+      return 1;
+   }
 };
-
 
 int main(void)
 {
-    std::cout << "SE3313 Chat Room Server" << std::endl;
-	std::cout << "Press enter to terminate the server...";
-    std::cout.flush();
-	
-    // Create our server
-    SocketServer server(3000);    
-    std::vector<SocketThread*> socketThreads; 
+   std::cout << "SE3313 Chat Room Server" << std::endl;
+   std::cout << "Press enter to terminate the server...";
+   std::cout.flush();
 
-    // Need a thread to perform server operations
-    ServerThread serverThread(server, socketThreads);
-	
-    // This will wait for input to shutdown the server
-    FlexWait cinWaiter(1, stdin);
-    cinWaiter.Wait();
+   // Create our server
+   SocketServer server(3000);
+   std::vector<SocketThread *> socketThreads;
 
-    // Shut down and clean up the server
-    server.Shutdown();
-    std::cout << "Server Exiting..." << std::endl;
+   // Need a thread to perform server operations
+   ServerThread serverThread(server, socketThreads);
 
+   // This will wait for input to shutdown the server
+   FlexWait cinWaiter(1, stdin);
+   cinWaiter.Wait();
+
+   // Shut down and clean up the server
+   server.Shutdown();
+   std::cout << "Server Exiting..." << std::endl;
 }
